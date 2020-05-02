f0727067d10ad3902324988cf1fdafc3c4c77ccc
Alexander Kiselev <agvarelex@gmail.com>
2020-05-02 17:31:25 +0300

    [SDC-1] video image processing
    Took 13 minutes

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ image_test_path = 'data/test_images/'
 image_output_path = 'data/test_images_output/'
 test_image_names = os.listdir(image_test_path)
 
+if not os.path.exists(image_output_path):
+    os.mkdir(image_output_path)
+
 for test_image_name in test_image_names:
     image_path = os.path.join(image_test_path, test_image_name)
     src = mpimg.imread(image_path)
@@ -17,6 +20,9 @@ for test_image_name in test_image_names:
 video_test_path = 'data/test_videos/'
 video_output_path = 'data/test_videos_output/'
 test_video_names = os.listdir(video_test_path)
+
+if not os.path.exists(video_output_path):
+    os.mkdir(video_output_path)
 
 for test_video_name in test_video_names:
     video_path = os.path.join(video_test_path, test_video_name)
